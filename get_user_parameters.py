43b5d36857d830d6c7004da94a8cc82f828b8e18
vfinel <victor.finel.pro@gmail.com>
2024-05-05 16:03:35 +0200

    comments some user parameters

--- get_user_parameters.py
+++ get_user_parameters.py
@@ -1,6 +1,6 @@
 def get_user_parameters():
     param = {
-            'project_file': "H:\\Mon Drive\\vico\\map\\map_experiments\\\map_20230701_correctionCRS\\nowhere2023 map.qgz",
+            'project_file': "H:\\Mon Drive\\vico\\map\\map_experiments\\map_20230701_correctionCRS\\nowhere2023 map.qgz", # this is not used and be ignored if the code is ran from QGIS console.
             'loadLayersList': ["norg2023_nodes", "art2023"],
             'cablesLayersList': ["norg2023_3phases", "norg2023_1phase","art2023_3phases", "art2023_1phase"],
             'spreadsheet': {
@@ -8,7 +8,7 @@ def get_user_parameters():
                 'sheet': "All",
                 'skiprows': 0
             },
-            'extra_cable_length': 10    # extra length to ad to each cable. 10 should be the minimum
+            'extra_cable_length': 10  # extra length to add to the "straight line length" to get the necessary length of the cable to have some slack. 10m should be the minimum
             }
         
     return param
