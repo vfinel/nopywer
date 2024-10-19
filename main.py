5f903805585492c1d4747214632343209c6caca7
vfinel <victor.finel.pro@gmail.com>
2024-10-19 15:23:49 +0200

    add comments to main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path += ['C:/PROGRA~1/QGIS33~1.3/apps/qgis/./python', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python/plugins', 'C:/PROGRA~1/QGIS33~1.3/apps/qgis/./python/plugins', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\grass\\grass83\\etc\\python', 'H:\\Mon Drive\\vico\\map\\map2023\\map_20230701_correctionCRS', 'C:\\PROGRA~1\\QGIS33~1.3\\bin\\python39.zip', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\DLLs', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib', 'C:\\PROGRA~1\\QGIS33~1.3\\bin', 'C:\\Users\\v.finel\\AppData\\Roaming\\Python\\Python39\\site-packages', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\win32', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\win32\\lib', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\Pythonwin', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'H:/Mon Drive/vico/map/map2023/map_20230701_correctionCRS'] #from sys.path ran from qgis' python console
-sys.path.append('H:\\Mon Drive\\vico\\map\\map2023\\nopywer') # to import nopywer modules from qgis' python cosole
+sys.path.append('H:\\Mon Drive\\vico\\map\\map2023\\nopywer') # to import nopywer from qgis' python console # this is annoying because hardcoded
 
 # imports
 import json 
@@ -20,12 +20,6 @@ from get_constant_parameters import get_constant_parameters
 from computeVDrop import computeVDrop
 from printGridInfo import printGridInfo
 from updateLayers import update1PhaseLayers, updateLoadLayers
-
-
-# tests (to reload code from QGIS python terminal ?)
-# import imp
-# import startup
-# imp.reload(startup)
 
 
 # --------------------------------------------------------- #
