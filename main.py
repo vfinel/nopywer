f6fe69df126cef082d42dd2766d0bf35d9f8a08e
vfinel <victor.finel.pro@gmail.com>
2025-03-30 11:18:35 +0200

    use conda to manage python env

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import sys
-sys.path += ['C:/PROGRA~1/QGIS33~1.3/apps/qgis/./python', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python/plugins', 'C:/PROGRA~1/QGIS33~1.3/apps/qgis/./python/plugins', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\grass\\grass83\\etc\\python', 'H:\\Mon Drive\\vico\\map\\map2023\\map_20230701_correctionCRS', 'C:\\PROGRA~1\\QGIS33~1.3\\bin\\python39.zip', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\DLLs', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib', 'C:\\PROGRA~1\\QGIS33~1.3\\bin', 'C:\\Users\\v.finel\\AppData\\Roaming\\Python\\Python39\\site-packages', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\win32', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\win32\\lib', 'C:\\PROGRA~1\\QGIS33~1.3\\apps\\Python39\\lib\\site-packages\\Pythonwin', 'C:/Users/v.finel/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'H:/Mon Drive/vico/map/map2023/map_20230701_correctionCRS'] #from sys.path ran from qgis' python console
-sys.path.append('H:\\Mon Drive\\vico\\map\\map2023\\nopywer') # to import nopywer from qgis' python console # this is annoying because hardcoded
-
-import json 
 from qgis.core import QgsApplication, QgsProject
 import qgis.utils
 import numpy as np
@@ -27,9 +22,7 @@ standalone_exec = __name__ == '__main__'
 if standalone_exec: # code is not ran from QGIS 
     # --- run qgis (to be able to run code from vscode - NOT HELPING)
     # Supply path to qgis install location
-    # QgsApplication.setPrefixPath("C:/Program Files/QGIS 3.34.3/apps/qgis/", True) # true=default paths to be used
     print('initializing QGIS...')
-    QgsApplication.setPrefixPath('C:/PROGRA~1/QGIS33~1.3/apps/qgis', True)
     qgs = QgsApplication([], False) # second argument to False disables the GUI.
     qgs.initQgis() # Load providers
 
