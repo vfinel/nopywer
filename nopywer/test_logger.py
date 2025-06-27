faa50f2e9d0bf48b8f354d4b8ef401cddceaa937
vfinel <victor.finel.pro@gmail.com>
2025-06-27 22:03:51 +0200

    improve test_logger fct to leverage function name display in logs

--- nopywer/test_logger.py
+++ nopywer/test_logger.py
@@ -1,7 +1,14 @@
 from nopywer.logger_config import logger
 
-logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
+
+def test_logger():
+    logger.trace("This is a trace message")
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
+
+
+if __name__ == "__main__":
+    test_logger()
