5948816160a24cdfcfb1e1af52360768a0a230d2
Victor Perron <victor@iso3103.net>
2026-03-03 14:37:33 +0100

    main: remove QGis ciompanion mode, keep standalone script

--- main.py
+++ main.py
@@ -1,111 +1,59 @@
-import traceback
 import os
+from pathlib import Path
 
 import numpy as np
-import qgis.utils
 from qgis.core import QgsApplication, QgsProject
-
-try:
-    from qgis.core import Qgis  # qgis 3 and above
-except ImportError:
-    from qgis.core import QGis as Qgis  # qgis 2 and below
 
 import nopywer as npw
 from nopywer.logger_config import logger
 
 
-def check_qgis_version():
-    qgis_version_min = "3.34"
-    qgis_version = Qgis.QGIS_VERSION.split(".")
+def load_qgis_project(project_file: str) -> QgsApplication:
+    logger.info("Initializing QGIS...")
+    qgs = QgsApplication([], False)
+    qgs.initQgis()
 
-    for idx, ver in enumerate(qgis_version_min.split(".")):
-        assert qgis_version[idx] >= ver, (
-            f"QGIS version should be at least {qgis_version_min} but is {Qgis.QGIS_VERSION}. Please update"
-        )
+    project_path = Path(project_file)
+    if not project_path.is_file():
+        raise FileNotFoundError(f'Project file does not exist: "{project_file}"')
 
-    return None
-
-
-def is_running_in_qgis():
-    """get QGS application instance"""
-    qgs = QgsApplication.instance()
-    running_in_qgis = qgs is not None
-    logger.info(f"running in QGIS = {running_in_qgis}")
-    return running_in_qgis
-
-
-def get_project(
-    param: dict, running_in_qgis: bool
-) -> tuple[QgsProject, str, QgsApplication]:
-    """get QGIS qgs_project and project file"""
     qgs_project = QgsProject.instance()
-    if running_in_qgis:
-        project_file = qgs_project.absoluteFilePath()
-        qgs = QgsApplication.instance()
+    if not qgs_project.read(project_file):
+        raise RuntimeError(f'Unable to read project "{project_file}"')
 
-    else:
-        project_file, qgs = load_qgis_project(param, qgs_project)
-
-    logger.info(f"project filename: {qgs_project.fileName()}\n")
-    project_folder = os.path.split(project_file)[0]
-
-    return qgs_project, project_folder, qgs
+    logger.info(f"Project: {qgs_project.fileName()}")
+    return qgs
 
 
-def load_qgis_project(
-    param: dict, qgs_project: QgsProject
-) -> tuple[str, qgis._core.QgsApplication]:
-    logger.info("initializing QGIS...")
-    qgs = QgsApplication([], False)  # second argument to False disables the GUI.
-    qgs.initQgis()  # Load providers
+def run_analysis(param: dict) -> tuple[dict, dict]:
+    qgs_project = QgsProject.instance()
+    project_folder = os.path.dirname(qgs_project.absoluteFilePath())
 
-    # load project - cf https://gis.stackexchange.com/questions/136861/getting-layer-by-name-in-pyqgis/136879#136879
-    logger.info("\nloading project...")
-    project_file = param["project_file"]
-    assert os.path.isfile(project_file), (
-        f'the project file does not exists: "{project_file}"'
-    )
-    status = qgs_project.read(project_file)
-    assert status, f'unable to read qgs_project "{project_file}"'
+    constants = npw.get_constant_parameters()
+    V0 = constants["V0"]
+    PF = constants["PF"]
 
-    return project_file, qgs
-
-
-def run_analysis(
-    qgs_project: QgsProject, project_folder: str, param: dict
-) -> tuple[dict, dict]:
-    # constant data (global variables)
-    CONSTANTS = npw.get_constant_parameters()
-    V0 = CONSTANTS["V0"]
-    PF = CONSTANTS["PF"]
-    cables_layers_list = param["cables_layers_list"]
-
-    # find grid geometry
     cables_dict, grid, dlist = npw.get_grid_geometry(qgs_project, param)
 
-    # spreadsheet: assign phases
-    # --> user manually assign phases via the spreadsheet
-    # TODO: use npw.phase_assignment_greedy(grid)
-
-    # load spreadsheet (power usage + phase) and add it to "grid" dictionnary
     grid, cables_dict, has_no_phase, sh = npw.read_spreadsheet(
         project_folder, grid, cables_dict, param["phase_balance_spreadsheet"]
     )
 
-    # compute cumulated current
     grid, cables_dict = npw.cumulate_current(grid, cables_dict, dlist, V0, PF)
 
     phase_balance = 100 * np.std(
         grid["generator"].cum_power / np.mean(grid["generator"].cum_power)
     )
 
-    cables_dict = npw.inspect_cable_layers(qgs_project, cables_layers_list, cables_dict)
+    cables_dict = npw.inspect_cable_layers(
+        qgs_project, param["cables_layers_list"], cables_dict
+    )
     grid = npw.compute_distro_requirements(grid, cables_dict)
 
-    logger.info("\ncomputingVDrop...")
+    logger.info("Computing voltage drop...")
     grid, cables_dict = npw.compute_voltage_drop(grid, cables_dict)
 
-    logger.info("\nchecking inventory:")
+    logger.info("Checking inventory...")
     npw.choose_cables_in_inventory(project_folder, cables_dict, param["inventory"])
     npw.choose_distros_in_inventory(project_folder, grid, param["inventory"])
 
@@ -121,30 +69,17 @@ def run_analysis(
     return grid, cables_dict
 
 
-def main() -> tuple[dict, dict, QgsApplication, bool]:
-    check_qgis_version()
-    running_in_qgis = is_running_in_qgis()
-    nopywer_folder = os.path.dirname(__file__)
-    logger.info(f"nopywer_folder: {nopywer_folder}")
-    if running_in_qgis:
-        os.chdir(nopywer_folder)
-
+def main():
     param = npw.get_user_parameters()
+    qgs = load_qgis_project(param["project_file"])
 
-    qgs_project, project_folder, qgs = get_project(param, running_in_qgis)
-    grid, cables_dict = run_analysis(qgs_project, project_folder, param)
-
-    logger.info("\nNopywer analysis completed :)")
-
-    return grid, qgs_project, qgs, running_in_qgis
-
-
-if (__name__ == "__main__") or (QgsApplication.instance() is not None):
     try:
-        grid, qgs_project, qgs, running_in_qgis = main()
-        if not running_in_qgis:
-            qgs.exitQgis()
+        grid, cables_dict = run_analysis(param)
+        logger.info("Nopywer analysis completed :)")
+        return grid, cables_dict
+    finally:
+        qgs.exitQgis()
 
-    except Exception as e:
-        # https://stackoverflow.com/questions/4990718/how-can-i-write-a-try-except-block-that-catches-all-exceptions/4992124#4992124
-        logger.error(traceback.format_exc())
+
+if __name__ == "__main__":
+    main()
